3e5f949145193096840ec8cb243cc0e5d0511052
Amer <amer.rasheed@gmail.com>
2020-04-06 14:48:43 +0200

    I added my name to FULL

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from Amer Rasheed !!" );
+        System.out.println( "Hello World from Amer Rasheed on my first day!!" );
     } //
 }
