eb535491d9e1c57aa26ce42ecddaf0cc16c926bd
Amer <amer.rasheed@gmail.com>
2020-04-07 16:37:32 +0200

    I added my name to FULL length

---
+++ src/main/java/org/example/Exercise.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+/**
+ * Hello world!
+ *
+ */
+public class Exercise
+{
+
+        public static void main(String[] args) {
+            byte b=127;
+            short s=-320;
+            long l=899;
+            float f=6.8f;
+            double d=23.5;
+            char c='w';
+            boolean b1=true;
+            int i=1;
+
+            System.out.println("Hello World");
+            /*System.out.println(b);
+            System.out.println(s);
+            System.out.println(l);
+            System.out.println(f);*/
+            System.out.println(d);
+            System.out.println(s);
+            System.out.println(b1);
+            System.out.println(++i); //Add FIRST and then increment unlike i++
+
+
+
+
+
+        }
+
+
+}
